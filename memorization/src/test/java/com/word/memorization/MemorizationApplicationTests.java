f2d0a5a80543764b859e19a6527af2a6b3e6db1a
Danil Krivobok <krivoboc.krivoboc@gmail.com>
2024-11-04 19:24:23 +0300

    First Commit

---
+++ memorization/src/test/java/com/word/memorization/MemorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.word.memorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
